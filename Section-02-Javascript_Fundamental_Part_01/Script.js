86b19f8521dd8e89ad6c920c46039b5983612717
AJAYKUMAR B S <ajaykumarbirursathish@gmail.com>
2024-09-02 21:15:10 -0700

    Fundamental_Part_01 : Learn about What is  values and variables, variable naming conventions, and reserved keywords.

--- Section-02-Javascript_Fundamental_Part_01/Script.js
+++ Section-02-Javascript_Fundamental_Part_01/Script.js
@@ -1,4 +1,37 @@
 let js = "amazing";
-if (js === "amazing") alert("Hello World!");
 44 + 23 + 12 - 9;
 console.log(44 + 23 + 12 - 9);
+
+let firstName = "Kumar"; //* This is The value that we store inside the firstName Variable.
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// * variables Naming Convention
+
+// let 3years =3 // ! it will throw the SyntaxError
+
+//let ajay&kumar = "ajay" // ! it will throw the SyntaxError
+
+let ajay_kumar = "BS"; //* This would be allowed
+
+//let new = 20 // ! this is also not allowed because "new" is Reserved keyword in javascript
+
+//let function ="ajay" // ! this  also not allowed because "function" is Reserved keyword in javascript
+
+let _new = 40; // * this will be allowed
+
+let _function = 50; // * this will be allowed
+
+let $new = 70; // * this will be allowed
+
+let $function = 80; // * this will be allowed
+
+console.log(_new, _function, $new, $function);
+
+let name = "Ajay"; // ! This will work but not recommended because "name" is also reserved keyword in javascript it leads to conflicts
+
+let PersonName = "John"; // ! This will work but not recommended because it is not the convention of variables upperCase is used for Constant values in
+
+let PI = 3.1415; // * This is Correct Conversion of Variables
